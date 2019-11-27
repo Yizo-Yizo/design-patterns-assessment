89ea493ff4fb7e354d858cf51d29721e06efe4d6
Yizo-Yizo <selebedi.thapelo6@gmail.com>
2019-11-27 08:52:55 +0200

    Main Activity Edited

--- src/exercise1/start/GreatQuotes.Android/MainActivity.cs
+++ src/exercise1/start/GreatQuotes.Android/MainActivity.cs
@@ -11,7 +11,6 @@ namespace GreatQuotes.Droid
     [Activity(Label = "@string/app_name", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        readonly SimpleContainer container = new SimpleContainer();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -19,10 +18,6 @@ namespace GreatQuotes.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
-            container.Register<IQuoteLoader, QuoteLoader>();
-            container.Register<ITextToSpeech, TextToSpeechService>();
-            container.Create<QuoteManager>();
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
